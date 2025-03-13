99d356bd2dc53409e5a54830e41705401f6d7b58
João Pedro <joaope.espindola2005@gmail.com>
2025-03-13 17:09:39 -0300

    Sending form data to controller

--- src/main/java/br/com/joaopedro/front_gestao_vagas/PrimeiraPaginaController.java
+++ src/main/java/br/com/joaopedro/front_gestao_vagas/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package br.com.joaopedro.front_gestao_vagas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class PrimeiraPaginaController {
  @GetMapping("/login")
  public String loginCandidante(){
   return "candidate/login";
+ }
+
+ @PostMapping("/create")
+ public String CadastroCandidato(String nome_do_candidato){
+  System.out.println("Nome do candidato " + nome_do_candidato);
+  return "/candidate/login";
  }
 }
