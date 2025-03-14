d0372b73b3270aae31090b0f091afc49a777e3d2
João Pedro <joaope.espindola2005@gmail.com>
2025-03-14 11:40:57 -0300

    RestTemplate for external API access

--- src/main/java/br/com/joaopedro/front_gestao_vagas/modules/Candidate/Controllers/CandidateController.java
+++ src/main/java/br/com/joaopedro/front_gestao_vagas/modules/Candidate/Controllers/CandidateController.java
@@ -1,16 +1,35 @@
-package br.com.joaopedro.front_gestao_vagas.modules.Candidate;
+package br.com.joaopedro.front_gestao_vagas.modules.Candidate.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@RequestMapping("/candidate")
-public class CandidateController {
+import br.com.joaopedro.front_gestao_vagas.modules.Candidate.service.CandidateService;
+ 
+ @Controller
+ @RequestMapping("/candidate")
+ public class CandidateController {
 
- @GetMapping("/login")
- public String login() {
-  return "candidate/login";
+     @Autowired
+     private CandidateService candidateService;
+ 
+     @GetMapping("/login")
+     public String login(){
+         return "candidate/login";
+     }
+
+     @PostMapping("/signIn")
+     public String signIn(RedirectAttributes redirectAttributes,String username, String password) {
+        try {
+        var token = this.candidateService.login(username, password);
+            return "candidate/profile";
+        } catch (HttpClientErrorException e) {
+            redirectAttributes.addFlashAttribute("error_message", "Usuário ou Senha incorretos!");
+            return "redirect:/candidate/login";
+        }
+     }
  }
-
-}
---
+++ src/main/java/br/com/joaopedro/front_gestao_vagas/modules/Candidate/dto/Token.java
@@ -0,0 +1,13 @@
+package br.com.joaopedro.front_gestao_vagas.modules.Candidate.dto;
+
+public class Token {
+ private String access_token;
+
+ public String getAcess_token() {
+  return access_token;
+ }
+
+ public void setAcess_token(String acess_token) {
+  this.access_token = acess_token;
+ }
+}
